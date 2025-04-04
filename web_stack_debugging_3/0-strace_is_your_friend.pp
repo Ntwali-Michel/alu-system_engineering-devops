47fdad994113d43abd20805972089f1b474b2252
mike-loen <m.ntwali@alustudent.com>
2025-04-04 06:04:57 +0000

    0-strace_is_your_friend.pp

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# puppet script idk
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+$file = '/var/www/html/wp-settings.php'
+exec { 'replace_phpp_with_php':
+  command => "sed -i 's/phpp/php/g' ${file}",
+  path    => ['/bin','/usr/bin']
 }
